6ae92eab49a733d8e9c97dc222ff2b5b29fc4818
daniel <daniel21dev@gmail.com>
2023-11-25 13:41:29 -0600

    fix: :sparkles: tests

--- src/modules/core/tasks/tasks.service.spec.ts
+++ src/modules/core/tasks/tasks.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { PrismaService } from '@/modules/shared/prisma/prisma.service';
 
 describe('TasksService', () => {
   let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
@@ -14,5 +21,37 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of tasks', async () => {
+      const result: Awaited<ReturnType<typeof service.findAll>> = [
+        {
+          id: '1',
+          title: 'Task 1',
+          description: 'Description 1',
+          status: 'TODO',
+          dueDate: new Date(),
+          cost: 1,
+          createdAt: new Date(),
+          timeEstimate: 1,
+          timeSpend: 1,
+          updatedAt: new Date(),
+          UserTask: [
+            {
+              user: {
+                id: '1',
+                name: 'User 1',
+                email: '',
+              },
+            },
+          ],
+        },
+      ];
+
+      jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+
+      expect(await service.findAll({ assigneeId: '1' })).toBe(result);
+    });
   });
 });
